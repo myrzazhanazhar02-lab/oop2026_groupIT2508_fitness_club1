81800d39abd2e23f3391e7771546fec0be89727f
tacoscacos-beep <tacoscacos@gmail.com>
2026-02-01 18:18:41 +0500

    make MemberRepository extend generic Repository

--- src/main/java/com/fitnessclub/repository/MemberRepository.java
+++ src/main/java/com/fitnessclub/repository/MemberRepository.java
@@ -3,15 +3,21 @@ package com.fitnessclub.repository;
 import com.fitnessclub.model.Member;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 
-public interface MemberRepository {
-    Member create(String name, String email, String phone, Integer membershipTypeId, LocalDate membershipEndDate);
+public interface MemberRepository extends Repository<Member, Integer> {
 
-    Optional<Member> findById(int id);
+    Member create(
+            String name,
+            String email,
+            String phone,
+            Integer membershipTypeId,
+            LocalDate membershipEndDate
+    );
 
-    List<Member> findAll();
-
-    Member updateMembership(int memberId, Integer membershipTypeId, LocalDate membershipEndDate);
+    Member updateMembership(
+            int memberId,
+            Integer membershipTypeId,
+            LocalDate membershipEndDate
+    );
 }
+
